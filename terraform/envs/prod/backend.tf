49f7fdefc773bda8bd598ea0cb0fe8a68997d107
unknown <pathaniaashutosh@gmail.com>
2026-02-06 22:46:48 +0530

    new branch

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "tf-state-252850512189"
     key    = "prod/terraform.tfstate"
-    region = "eu-west-2"
+    region = "us-east-1"
 
     # Recommended in real AWS
     encrypt = true
